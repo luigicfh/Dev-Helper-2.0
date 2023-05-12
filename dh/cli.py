63845f2ecbe64c063a8a7ef106a132ec496748e2
luigicfh <66189982+luigicfh@users.noreply.github.com>
2023-05-12 08:59:46 -0600

    beta testing

--- dh/cli.py
+++ dh/cli.py
@@ -1,49 +1,20 @@
+#!/usr/bin/env python3
 import argparse
-import re
-from backend.document_db import DocumentDatabase
+from .backend.document_db import DocumentDatabase
+from .backend.validations import *
 import os
 import yaml
+import openai
+import markdown
+import streamlit as st
+from pygments import highlight
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters import TerminalFormatter
 
-cwd = os.getcwd()
-document_db_path = os.path.join(cwd, "backend", "db.json")
 
-def validate_list_input(namespace:str) -> list:
-    if namespace == "default":
-        return []
-    if "." not in namespace:
-        return [namespace]
-    if re.match(r'^[a-z]+\.[a-z]+$', namespace) is not None:
-        return namespace.split(".")
-    return []
-
-def validate_add_input(project:str, name:str):
-    if not project.islower() and not project.isalpha(): 
-        return False, """Project names must be lowercase 
-        and only include alphabetical characters.""",
-    if len(project) > 8:
-        return False, """Project names can't exceed 8 
-        characters in length."""
-    if not name.islower() and not name.isalpha():
-        return False, """Shortcut command names must be 
-        lowercase and only include alphabetical characters."""
-    return True, "Valid inputs."
-
-def validate_delete_input(project:str):
-    if project is None:
-        raise ValueError("Provide a project name to delete.")
-    return True
-
-def validate_update_input(project:str, name:str, command:str):
-    if project is None and name is None and command is None:
-        raise ValueError(
-        "Please provide the project and shortcut to update")
-    return True
-
-def validate_run_input(name:str):
-    if name is None:
-        raise ValueError(
-        "Please provide a shortcut name.")
-    return
+module_directory = os.path.dirname(__file__)
+document_db_path = os.path.join(module_directory, "backend", "db.json")
+openai.api_key = os.environ.get("API_KEY")
 
 def generate_new_command(name: str, desc:str, command:str):
     return {
@@ -51,6 +22,38 @@ def generate_new_command(name: str, desc:str, command:str):
         "description": desc,
         "command": command
     }
+
+def chat_with_gpt(prompt):
+    response = openai.Completion.create(
+        engine='gpt-3.5-turbo',
+        prompt=prompt,
+        max_tokens=100,
+        n=1,
+        stop=None,
+        temperature=0.7,
+    )
+    output = response.choices[0].text.strip()
+    response_type = response.choices[0].type
+
+    if response_type == 'text':
+        print(output)
+    elif response_type == 'code':
+        display_code_as_markdown(output, "python")
+    else:
+        print(output)
+
+
+def render_markdown(markdown_text):
+    st.title("Markdown Renderer")
+    html = markdown.markdown(markdown_text)
+    st.markdown(html, unsafe_allow_html=True)
+
+def display_code_as_markdown(code, language):
+    lexer = get_lexer_by_name(language)
+    formatter = TerminalFormatter()
+    highlighted_code = highlight(code, lexer, formatter)
+    print(highlighted_code)
+
 
 def parse():
     commands = [
@@ -60,20 +63,17 @@ def parse():
             'update',
             'run', 
             'config', 
-            'switch', 
-            'ask'
+            'switch'
         ]
     parser = argparse.ArgumentParser()
     parser.add_argument("cmd", 
         choices=commands,
     )
     parser.add_argument('-namespace', '-n', type=str, default="default")
-    # Adding filters later
-    # parser.add_argument('-filter', '-f', type=str)
-    parser.add_argument('-project', '-pr', type=str, default=None)
+    parser.add_argument('-project', '-p', type=str, default=None)
     parser.add_argument('-description', '-d', type=str)
     parser.add_argument('-command', '-c', type=str, default=None)
-    parser.add_argument('-ask', '-a', type=str)
+    # parser.add_argument('-question', '-q', type=str)
     parser.add_argument("-shortcut", "-s", type=str, default=None)
     args = parser.parse_args()
     db = DocumentDatabase(document_db_path)
@@ -98,15 +98,15 @@ def parse():
         if not input_valid[0]:
             raise Exception(input_valid[1])
         project = db.get_document(args.project)
-        new_command = generate_new_command(args.shortcut, args.description, args.command)
+        new_command = generate_new_command(
+            args.shortcut, args.description, args.command)
         if project is None:
             commands = {
                 "commands": [new_command]
             }
             db.add_document(args.project, commands)
             db.save()
-            print(
-            f"New command {args.shortcut} added successfuly.")
+            print(f"New command {args.shortcut} added successfuly.")
             return
         commands = project.get("commands")
         existing_command_names = [cmd["name"] for cmd in commands]
@@ -117,8 +117,7 @@ def parse():
         project['commands'] = commands
         db.update_document(args.project, project)
         db.save()
-        print(
-        f"New command {args.shortcut} added successfuly.")
+        print(f"New command {args.shortcut} added successfuly.")
         return
 
     if args.cmd == commands[2]:
@@ -139,8 +138,7 @@ def parse():
         project["commands"] = commands
         db.update_document(args.project, project)
         db.save()
-        print(
-        f"Command {args.shortcut} deleted successfuly.")
+        print(f"Command {args.shortcut} deleted successfuly.")
         return
     
     if args.cmd == commands[3]:
@@ -161,14 +159,13 @@ def parse():
         project["commands"] = commands
         db.update_document(args.project, project)
         db.save()
-        print(
-        f"Command {args.shortcut} updated successfuly.")
+        print(f"Command {args.shortcut} updated successfuly.")
         return
     
     if args.cmd == commands[4]:
         validate_run_input(args.shortcut)
         active_config = db.data.get("activeConfig")
-        if "project" not in active_config.keys():
+        if active_config is None:
             print("It seems that you have not activated a project.")
             project = input(
             f"Please provide the project name of the command {args.shortcut}: ")
@@ -176,15 +173,36 @@ def parse():
                 raise ValueError("Please provide a valid project name.")
             if project not in db.data.keys():
                 raise ValueError(f"Project {project} does not exist.")
-            db.update_document("activeConfig", {"project": project})
+            db.add_document("activeConfig", {"project": project})
             db.save()
         active_project = active_config.get("project")
         project = db.get_document(active_project)
         commands = project.get("commands")
         existing_command_names = [cmd["name"] for cmd in commands]
+        if args.shortcut not in existing_command_names:
+            raise ValueError(f"Command does not exist in project {args.project}")
         index_of_command = existing_command_names.index(args.shortcut)
         command = commands[index_of_command].get("command")
         os.system(command)
+        return
+
+    if args.cmd == commands[5]:
+        active_config = db.data.get("activeConfig")
+        print(yaml.dump(active_config))
+
+    if args.cmd == commands[6]:
+        if args.project is None:
+            raise ValueError(
+            "Please provide a project to set the active configuration.")
+        active_config = db.data.get("activeConfig")
+        project = db.get_document(args.project)
+        if project is None:
+            raise ValueError(f"Project {project} does not exist.")
+        active_config["project"] = args.project
+        db.update_document("activeConfig", active_config)
+        db.save()
+        print(f"Your active project is {args.project}")
+        return
 
 if __name__ == '__main__':
     parse()
